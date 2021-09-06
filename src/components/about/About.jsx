949e58b7a5ed8fa76e3f4915c395c8138c359bac
estePecora <b.estefaniapecora@gmail.com>
2021-09-06 20:33:16 -0300

    added/projectCards

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,7 +6,11 @@ const About = () => {
         <div className='about-container'>
             <div className='titles'>About Me</div>
             <div className='about-description'> 
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi consectetur beatae ipsam mollitia fugiat laudantium non dicta laboriosam. Ipsam laborum minus natus fuga quod. Accusantium excepturi distinctio quidem officia est?</p>
+                <p>I’m a responsible and proactive person, 
+                    I enjoy helping others and I’m always willing to learn and develop new skills.
+                    I’ve worked in advertising for several years, but I’ve always been interested in technology,
+                    and how it improves people's lives. When I started learning to code I realized it combined two of my favorite things: technology and creative thinking, 
+                    so I decided to change the course of my professional path. If you like my profile leave me a message!</p>
             </div>
             
         </div>
--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import './Work.css'
+import HardwareStoreImg from '../../media/felix-besombes-EzrHLVd83h0-unsplash.jpg'
+import CountryAppImg from '../../media/chuttersnap-QkPb5g9p338-unsplash.jpg'
+
+const projectInfo = [
+{   
+    weblink: 'https://hardwarestore.vercel.app/',
+    gitlink: 'https://github.com/estePecora/FP-E-Commerce',
+    projectImage: HardwareStoreImg,
+    projectName: 'HardwareStore',
+    projectDescription: 'This project was made using Nodejs, Express, React-Redux'
+},
+{
+    weblink: 'https://hardwarestore.vercel.app/',
+    gitlink: 'https://github.com/estePecora/Countries-App',
+    projectImage: CountryAppImg,
+    projectName: 'CountryApp',
+    projectDescription: 'This project was made using Nodejs, Express, React-Redux'
+},
+]
 
 const Work = () => {
+    
     return (
         <div className='work-container'>
-            
+            {projectInfo.map(project => {
+                return (
+                    <div className='main-project-container'>      
+                        <div className='project-container'>
+                            <a href={project.weblink} target='_blank' rel="noreferrer">
+                            <img src={project.projectImage} className='project-images'/>
+                            <h3 className='project-titles'>{project.projectName}</h3>
+                            </a>
+                            <div className='project-descriptions'>{project.projectDescription}</div>
+                            <span className='project-git-link'>
+                            <a href={project.gitlink} target='_blank' rel="noreferrer">
+                                <i className='fab fa-github github-project'></i>
+                            </a>
+                            </span>
+                        </div>
+                    </div> 
+                )
+            })} 
         </div>
     )
 }
